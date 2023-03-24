6f5f3ee80010df3c3b3b84015a8730f8d16a20a5
AmarnathCKR <ckramarnath@gmail.com>
2023-03-24 13:39:18 +0530

    final commit success

--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 //Set URI
-const URI = process.env.MONGODB_URI || "mongodb://0.0.0.0:27017/sdeProject";
+const URI = process.env.MONGODB_URI || "mongodb+srv://ckramarnath:Qhq22UVmCQvyeV7y@sdeproject.1fq7t6m.mongodb.net/?retryWrites=true&w=majority";
 //Store Connection Object
 const db = mongoose.connection;
 //Config Object to Avoid Deprecation Warnings
